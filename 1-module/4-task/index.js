072709281c2748fe80295419444b1fc012d9d2ca
Maxim Feofanov <maximfeofanov@icloud.com>
2022-12-21 21:25:37 +0300

    Homework 21.12.2022

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  if (str.toLowerCase() === "1xbet now" || str.toLowerCase() === "free xxxxx") {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
