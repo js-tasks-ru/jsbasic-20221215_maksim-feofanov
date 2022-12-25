08fe4b7b937a4a330018d2f1122d6a1f409776ae
Maxim Feofanov <maximfeofanov@icloud.com>
2022-12-25 19:37:53 +0300

    2 module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let summa = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === "number" && !isNaN(salaries[key]) && salaries[key] !== Infinity && salaries[key] !== -Infinity) {
+      summa += salaries[key];
+    }
+  }
+  return summa;
 }
